14d8fda3e7fa3a367894be3815c1d88e5fa4d159
Renan-Laba <bottrenan158@gmail.com>
2022-05-13 11:28:53 -0300

    Terceira Versão

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+//Comentário
+/* Comentário */
 function teste(){
-    document.getElementById("info").innerHTML = "Esse é um texto em JavaScript";
+
+    //var idade = 19;
+    var preco = 12.5;
+    var x = 10 + 30;
+    var y = x + 20;
+
+    //var nome = "Renan", sobrenome = 'Laba', idade = 19;
+    var nome = window.prompt("Informe seu nome: ");
+    var sobrenome = window.prompt("Informe seu sobrenome: ");
+    var idade = window.prompt("Informe sua idade: ");
+
+    window.alert("Olá, "+ nome + " " + sobrenome + ". Você tem " + idade+ " anos!")
+
+    document.getElementById("info").innerHTML = "Olá, "+ nome + " " + sobrenome + ". Você tem " + idade+ " anos!";
 }
